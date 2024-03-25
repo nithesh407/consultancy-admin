0526b46b87abd9e27be9db5f06e63351b18e0988
nithesh407 <nitheshravikumar13631@gmail.com>
2024-03-25 23:24:04 +0530

    modified theme in the admin panel

--- src/pages/Layout/RootLayout.tsx
+++ src/pages/Layout/RootLayout.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Layout } from 'antd';
 import { Outlet } from 'react-router-dom';
-import { Products, Account, CustomerDetails, Orders } from '..';
 import { HeaderComponent, SiderComponent } from '../../Components';
 
 
